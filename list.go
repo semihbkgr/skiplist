dec7bdf9c6bf5f613b8ecd86f4feb73d0aca0ef2
semihbkgr <buyukgungorsemih@gmail.com>
2023-06-28 22:48:29 +0300

    add missing operations and unit tests

--- list.go
+++ list.go
@@ -1,9 +1,15 @@
 package skiplist
 
 import (
-	"golang.org/x/exp/constraints"
 	"math/rand"
 	"time"
+
+	"golang.org/x/exp/constraints"
+)
+
+const (
+	defaultMaxLevel = 12
+	minimumMaxLevel = 0
 )
 
 type node[K constraints.Ordered, V any] struct {
@@ -23,30 +29,29 @@ func newNode[K constraints.Ordered, V any](key K, val V, level int) *node[K, V] 
 type SkipList[K constraints.Ordered, V any] struct {
 	head     *node[K, V]
 	level    int
-	size     int
+	len      int
 	maxLevel int
 	rand     *rand.Rand
 }
 
-func NewSkipList[K constraints.Ordered, V any](maxLevel int) *SkipList[K, V] {
+func New[K constraints.Ordered, V any](maxLevel ...int) *SkipList[K, V] {
+	maxLvl := defaultMaxLevel
+	if len(maxLevel) > 0 {
+		maxLvl = maxLevel[0]
+	}
+	if maxLvl < minimumMaxLevel {
+		maxLvl = minimumMaxLevel
+	}
 	return &SkipList[K, V]{
-		head:     newNode[K, V](*new(K), *new(V), maxLevel),
+		head:     newNode[K, V](*new(K), *new(V), maxLvl),
 		level:    0,
-		size:     0,
-		maxLevel: maxLevel,
+		len:      0,
+		maxLevel: maxLvl,
 		rand:     rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
 }
 
-func (l *SkipList[K, V]) nextLevel() int {
-	level := 0
-	for level < l.maxLevel && l.rand.Uint32()%2 == 0 {
-		level++
-	}
-	return level
-}
-
-func (l *SkipList[K, V]) Find(key K) (V, bool) {
+func (l *SkipList[K, V]) Get(key K) (V, bool) {
 	n := l.head
 	for i := l.level; i >= 0; i-- {
 		for n.next[i] != nil && n.next[i].key < key {
@@ -60,8 +65,16 @@ func (l *SkipList[K, V]) Find(key K) (V, bool) {
 	return n.val, true
 }
 
+func (l *SkipList[K, V]) randLevel() int {
+	level := 0
+	for level < l.maxLevel && l.rand.Uint32()%2 == 0 {
+		level++
+	}
+	return level
+}
+
 func (l *SkipList[K, V]) Insert(key K, val V) {
-	nodeLevel := l.nextLevel()
+	nodeLevel := l.randLevel()
 	if l.level < nodeLevel {
 		l.level = nodeLevel
 	}
@@ -78,5 +91,29 @@ func (l *SkipList[K, V]) Insert(key K, val V) {
 		n.next[i] = levelTraceNodes[i].next[i]
 		levelTraceNodes[i].next[i] = n
 	}
-	l.size++
+	l.len++
+}
+
+func (l *SkipList[K, V]) Delete(key K) bool {
+	levelTraceNodes := make([]*node[K, V], l.level+1)
+	n := l.head
+	for i := l.level; i >= 0; i-- {
+		for n.next[i] != nil && n.next[i].key < key {
+			n = n.next[i]
+		}
+		levelTraceNodes[i] = n
+	}
+	n = n.next[0]
+	if n == nil || n.key != key {
+		return false
+	}
+	for i, next := range n.next {
+		levelTraceNodes[i].next[i] = next
+	}
+	l.len--
+	return true
+}
+
+func (l *SkipList[K, V]) Length() int {
+	return l.len
 }
---
+++ list_test.go
@@ -0,0 +1,103 @@
+package skiplist
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestNew(t *testing.T) {
+	l := New[int, string]()
+	assert.NotNil(t, l)
+	assert.Equal(t, defaultMaxLevel, l.maxLevel)
+}
+
+func TestNew_maxLevel(t *testing.T) {
+	maxLevel := 18
+
+	l := New[int, string](maxLevel)
+	assert.NotNil(t, l)
+	assert.Equal(t, maxLevel, l.maxLevel)
+}
+
+func TestNew_negativeMaxLevel(t *testing.T) {
+	maxLevel := -3
+
+	l := New[int, string](maxLevel)
+	assert.NotNil(t, l)
+	assert.Equal(t, minimumMaxLevel, l.maxLevel)
+}
+
+func TestSkipList_Insert(t *testing.T) {
+	l := New[int, string]()
+
+	for i := 0; i < 100; i++ {
+		v := fmt.Sprintf("%v-%d", time.Now(), i)
+		l.Insert(i, v)
+	}
+}
+
+func TestSkipList_Get(t *testing.T) {
+	l := New[int, string]()
+
+	for i := 0; i < 100; i++ {
+		v, ok := l.Get(i)
+		assert.False(t, ok)
+		assert.Empty(t, v)
+	}
+
+	kvMap := make(map[int]string)
+	for i := 0; i < 100; i++ {
+		v := fmt.Sprintf("%v-%d", time.Now(), i)
+		l.Insert(i, v)
+		kvMap[i] = v
+	}
+
+	for i := 0; i < 100; i++ {
+		v, ok := l.Get(i)
+		assert.True(t, ok)
+		assert.Equal(t, v, kvMap[i])
+	}
+}
+
+func TestSkipList_Delete(t *testing.T) {
+	l := New[int, string]()
+
+	for i := 0; i < 100; i++ {
+		ok := l.Delete(i)
+		assert.False(t, ok)
+	}
+
+	for i := 0; i < 100; i++ {
+		v := fmt.Sprintf("%v-%d", time.Now(), i)
+		l.Insert(i, v)
+	}
+
+	for i := 0; i < 100; i++ {
+		ok := l.Delete(i)
+		assert.True(t, ok)
+	}
+}
+
+func TestSkipList_Length(t *testing.T) {
+	l := New[int, string]()
+
+	length := l.Length()
+	assert.Zero(t, length)
+
+	for i := 0; i < 100; i++ {
+		v := fmt.Sprintf("%v-%d", time.Now(), i)
+		l.Insert(i, v)
+	}
+
+	length = l.Length()
+	assert.Equal(t, 100, length)
+
+	for i := 0; i < 100; i++ {
+		l.Delete(i)
+	}
+
+	length = l.Length()
+	assert.Zero(t, length)
+}
